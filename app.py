db3aa31ddb99338137686f84f7f3464baa49f732
tanmay mondal <119605098+monds1320@users.noreply.github.com>
2024-06-09 18:50:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -26,4 +26,4 @@ def predict():
         return render_template("index.html", error="Please enter valid values.")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0" , port=80)
